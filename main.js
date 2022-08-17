578b7c9a5a924aa1b9ad68c216d3391a71c1634f
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-08-17 09:38:26 +0900

    YAY

---
+++ main.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('a');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    let ripple = document.createElement('span');
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+    e.target.appendChild(ripple);
+    setTimeout(() => {
+      ripple.remove();
+    }, 600);
+  });
+});
